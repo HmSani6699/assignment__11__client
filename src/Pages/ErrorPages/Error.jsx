827ba3654ee94ed958ebb88910345bed7520f374
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-19 07:37:21 +0600

    creat to error page

--- src/Pages/ErrorPages/Error.jsx
+++ src/Pages/ErrorPages/Error.jsx
@@ -1,8 +1,10 @@
-
+import error from '../../assets/download (1).png'
 const Error = () => {
     return (
-        <div>
-            <h2>Ami error maa</h2>
+        <div className='mt-20 flex items-center justify-center '>
+            <div className='w-[400px]'>
+                <img className='w-full' src={error} alt="" />
+            </div>
         </div>
     );
 };
