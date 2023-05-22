43ebca0ef2fe68037cd50c5d7742703637da0ae7
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-23 00:11:47 +0600

    added to back button in error page

--- src/Pages/ErrorPages/Error.jsx
+++ src/Pages/ErrorPages/Error.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
 import error from '../../assets/download (1).png'
 const Error = () => {
     return (
-        <div className='mt-20 flex items-center justify-center '>
+        <div className='mt-16 flex items-center justify-center '>
             <div className='w-[400px]'>
                 <img className='w-full' src={error} alt="" />
+                <div className='text-center'>
+                    <Link to='/'> <button className="btn  text-white bg-[#c2410c]">Back to Home</button></Link>
+                </div>
             </div>
         </div>
     );
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
       },
       {
         path: '/myToys',
-        element: <MyToys></MyToys>
+        element: <PrivetRouts><MyToys></MyToys></PrivetRouts>
       },
       {
         path: '/blog',
