9447ca38da07f2e182896b6f9e9be50085a89135
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-22 23:05:46 +0600

    added to my toys component

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -9,6 +9,7 @@ import PrivetRouts from "./PrivetRouts";
 import AllToys from "../Pages/AllToys/AllToys";
 import Blog from "../Pages/Blog/Blog";
 import ToyDertails from "../Pages/ToyDetails/ToyDertails";
+import MyToys from "../Pages/MyToys/MyToys";
 
 
 
@@ -40,6 +41,10 @@ const router = createBrowserRouter([
       {
         path: '/addToy/:id',
         element:<PrivetRouts><ToyDertails></ToyDertails></PrivetRouts>
+      },
+      {
+        path:'/myToys',
+        element:<MyToys></MyToys>
       },
       {
         path: '/blog',
