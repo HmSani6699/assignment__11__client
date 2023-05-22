65cad041ceefe626f90fe011fa7aebece885e5d7
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-22 22:37:25 +0600

    display show the toy details in modal

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -1,18 +1,30 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 
 const AllToys = () => {
 
     const [allToys, setAlltoys] = useState([]);
+    const [toyDeails, setToyDeails] = useState({})
 
     useEffect(() => {
         fetch('http://localhost:5000/addToy')
             .then(res => res.json())
             .then(data => {
-                setAlltoys(data.slice(0,20));
+                setAlltoys(data.slice(0, 20));
             })
-    }, [])
+    }, []);
 
+
+    const handleModal = (id) => {
+        console.log(id);
+
+        fetch(`http://localhost:5000/addToy/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                setToyDeails(data);
+            })
+    }
+
+    console.log(toyDeails);
 
     return (
         <div>
@@ -33,13 +45,34 @@ const AllToys = () => {
                     </thead>
                     <tbody>
                         {
-                            allToys.map((toy,i) => <tr className="text-center" key={toy._id}>
-                                <th>{i+1}</th>
-                                <td>{toy.Seller}</td>
+                            allToys.map((toy, i) => <tr className="text-center" key={toy._id}>
+                                <th>{i + 1}</th>
+                                <td>{toy.seller}</td>
                                 <td>Quality Control Specialist</td>
                                 <td>{toy.category}</td>
                                 <td>{toy.price}</td>
-                                <td><Link to={`/addToy/${toy._id}`}><button className="btn text-white bg-[#c2410c] btn-outline">View Details</button> </Link></td>
+                                {/* <td><Link to={`/addToy/${toy._id}`}><button className="btn text-white bg-[#c2410c] btn-outline">View Details</button> </Link></td> */}
+                                <td>
+                                    <label htmlFor="my-modal-3" onClick={() => handleModal(toy._id)} className="btn">View Details</label>
+                                    <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+                                    <div className="modal">
+                                        <div className="modal-box relative h-[500px] w-[600px] ">
+                                            <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                                            <div className="flex items-center justify-center ">
+                                                <img className="h-[200px] mb-4" src={toyDeails?.photo} alt="img nai" />
+                                            </div>
+                                            <h3 className="font-bold">{toyDeails?.name}</h3>
+                                            <p><span className="font-bold">Sealler name</span> : {toyDeails.seller}</p>
+                                            <p><span className="font-bold">Seller email</span> : {toyDeails?.email}</p>
+                                            <p><span className="font-bold">Price</span> : {toyDeails?.price}</p>
+                                            <p><span className="font-bold">Rating</span> : {toyDeails?.rating}</p>
+                                            <p><span className="font-bold">Available quantity</span> : {toyDeails?.quantity}</p>
+                                            <p><span className="font-bold w-[100px]">Detail description</span> : {toyDeails?.details}</p>
+
+                                        </div>
+                                    </div>
+                                </td>
+
                             </tr>)
                         }
 
--- src/Pages/ToyDetails/ToyDertails.jsx
+++ src/Pages/ToyDetails/ToyDertails.jsx
@@ -1,22 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-const ToyDertails = () => {
+const ToyDertails = ({id}) => {
 
-    const id =useParams();
-    console.log(id.id);
+    const [toy, setToy] = useState()
+    console.log(toy);
 
-    useEffect(()=>{
-        fetch(`http://localhost:5000/addToy/${id.id}`)
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data);
-        })
-    },[])
+    // const id = useParams();
+
+    // useEffect(() => {
+    //     // fetch(`http://localhost:5000/addToy/${id.id}`)
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             setToy(data);
+    //         })
+    // }, [])
 
     return (
         <div>
-            <h2>Toy details page</h2>
+            {/* Put this part before </body> tag */}
+            <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box relative">
+                    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
+                    <p className="py-4">ve been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores repudiandae possimus itaque unde totam eius ut, nostrum, quaerat tempora maiores consectetur odit temporibus earum optio ad doloribus aspernatur cupiditate labore qui hic nulla. Porro, ratione officiis repellendus rem, consectetur magni culpa corrupti vel autem ex sint numquam et beatae. Amet sint minima dolorem quibusdam ipsum beatae voluptatem quam fugiat velit quas. Accusamus numquam inventore aspernatur deleniti ratione reprehenderit sequi quo nulla esse excepturi ab, et eligendi non officia temporibus veniam deserunt eveniet soluta. Ratione numquam perferendis aspernatur debitis error! Dolores vitae maiores delectus autem unde porro iure quasi maxime adipisci doloremque labore reprehenderit, cumque culpa amet quia! Eum molestias iure illo consectetur doloremque inventore ex reprehenderit blanditiis temporibus dicta! Perspiciatis ipsa aliquam aperiam consectetur quisquam suscipit dolorum rerum. Beatae sunt quaerat debitis aliquam et aperiam, soluta earum. Fugiat amet consectetur nihil voluptatibus cum deleniti, quam ullam, et officiis reiciendis quas! Dolorum, dolor modi! Facilis et magni dolores, voluptatum quos molestias eius, architecto dignissimos earum iure ex? Quo rem iste voluptatum ea culpa repellat facilis cum maxime perspiciatis ad excepturi cumque repellendus dolores esse, debitis molestiae facere inventore, aspernatur iure? Molestiae aliquam perferendis nobis sunt ad magnam pariatur? Labore, harum quaerat.</p>
+                </div>
+            </div>
         </div>
     );
 };
